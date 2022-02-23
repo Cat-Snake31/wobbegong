3e19283ac3352719fe54d96e2e6578a10ff19d48
Frank Nguyen <frankknguyen@gmail.com>
2022-02-23 15:21:43 -0600

    added tests for '/recipes' GET and POST

--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -1,7 +1,35 @@
 const request = require('supertest');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Recipe = require('../server/models/RecipeModel.js');
+const User = require('../server/models/UserModel.js');
 
 const server = 'http://localhost:3000';
 
+function deleteRecipe() {
+  Recipe.findOneAndDelete({ name: 'JESTtestUniqueName', query: 'blueberry' }, (err, result) => {
+    if (err){
+      console.log('deleteRecipe error ', err);
+    } else {
+      console.log('deleted recipe: ', result);
+    }
+  });
+  return
+}
+
+function getTestID() {
+  Recipe.findOne({ name: 'JESTtestUniqueName', query: 'blueberry' }, (err, result) => {
+    if (err){
+      console.log('getTestID error ', err);
+    } else {
+      console.log('result of getTestID ', result._id)
+    }
+  })
+  return
+}
 describe('Route Integration', () => {
   describe('/', () => {
     describe('GET', () => {
@@ -14,7 +42,7 @@ describe('Route Integration', () => {
     })
   });
   describe('/recipes', () => {
-    const testRecipe = { name: 'JESTtest', query: 'blueberry' };
+    const testRecipe = { name: 'JESTtestUniqueName', query: 'blueberry' };
     
     describe('GET', () => {
       it('responds with 200 status and application/json content type', () => {
@@ -39,16 +67,30 @@ describe('Route Integration', () => {
           .send(testRecipe)
           .expect('Content-Type', /application\/json/)
           .expect(200)
+          .then(response => {
+            deleteRecipe();
+          })
       });
       it('new recipe is in the body of the response', () => {
         return request(server)
           .post('/recipes')
           .send(testRecipe)
           .then((response) => {
+            // deleteRecipe();
+            getTestID();
             expect(response.body).toEqual(
-              expect.objectContaining({name: 'JESTtest'}),
+              expect.objectContaining({name: 'JESTtestUniqueName'}),
               expect.objectContaining({query: 'blueberry'})
             )
+          })
+      });
+      it('responds to invalid request with 400 status and error message in body', () => {
+        return request(server)
+          .post('/recipes')
+          .send('invalid request')
+          .expect(400)
+          .then(res => {
+            expect(res.error).not.toEqual(false);
           })
       });
     });
@@ -60,24 +102,54 @@ describe('Route Integration', () => {
         name: 'recipename',
         query: 'ingredient1, ingredient2, etc'
       } */
-    xdescribe('PUT', () => {
-      it('responds with 200 status and application/json content type', () => {
+    describe('PUT', () => {
+      xit('responds with 200 status and application/json content type', () => {
         return request(server)
           .put('/recipes')
           .send(testRecipe)
           .expect('Content-Type', /application\/json/)
           .expect(200)
       });
+      it('responds to invalid request with 400 status and error message in body', () => {
+        return request(server)
+          .put('/recipes')
+          .send('invalid request')
+          .expect(400)
+          .then(res => {
+            expect(res.error).not.toEqual(false);
+          })
+      });
     })
 
-    xdescribe('DELETE', () => {
-      it('responds with 200 status', () => {
+    describe('DELETE', () => {
+      xit('responds with 200 status', () => {
         return request(server)
           .delete('/recipes')
           .send(toBeDeleted)
           .expect(200)
       })
+      it('responds to invalid request with 400 status and error message in body', () => {
+        return request(server)
+          .delete('/recipes/200')
+          .expect(400)
+          .then(res => {
+            expect(res.error).not.toEqual(false);
+          })
+      });
     })
   })
+  describe('/users', () => {
+    xdescribe('GET', () => {
 
+    })
+    xdescribe('POST', () => {
+      
+    })
+    xdescribe('PUT', () => {
+      
+    })
+    xdescribe('DELETE', () => {
+      
+    })
+  })
 })
